25f9a0727dde3996007514788bfae04bbc588a1f
kinsey <jingsai.lu@nxp.com>
2017-02-12 20:03:47 +0800

    seperate event from html

--- image_lib/js/ShowPic.js
+++ image_lib/js/ShowPic.js
@@ -1,15 +1,41 @@
+function prepareGallery()
+{
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	if (!document.getElementById("imageGallery")) {return false};
+	var gallery = document.getElementById("imageGallery");
+	var links = gallery.getElementsByTagName("a");
+	for(var i = 0; i < links.length; i++)
+	{
+		links[i].onclick = function(){
+			showPic(this); return false;
+		}
+	}
+}
+
 function showPic(whichPic)
 {
+	if (!document.getElementById("placeholder")) {return false};
+
 	var source = whichPic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
+	if (placeholder.nodeName != "IMG") {return false};
+
 	placeholder.setAttribute("src", source);
 
-	var text = whichPic.getAttribute("title");
-	var description = document.getElementById("description");
+	if (document.getElementById("description")) {
+		var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+		var description = document.getElementById("description");
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text;
+		};
+	};
+	return true;
+	
 	// alert(description.childNodes[0].nodeValue);
 
 	// description.childNodes[0].nodeValue = text;
-	description.firstChild.nodeValue = text;
+	
 }
 
 function countBodyChildren()
@@ -20,3 +46,5 @@ function countBodyChildren()
 }
 
 // window.onload = countBodyChildren;
+
+window.onload = prepareGallery;
